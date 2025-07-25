ffa8c478aafdee4331de1e866ccff7a4071e7af8
jmmarketing <jeff@jeffreymclean.com>
2025-07-25 15:48:08 -0700

    Created runCountdown function to run timer and update countdonw cards. timer variable sets an interval to operate using runCountdown function.

--- src/js/script.js
+++ src/js/script.js
@@ -26,11 +26,25 @@ const secCard = document.querySelector("#sec");
 
 const endDate = new Date("2025-11-04T00:00:00");
 
+const timer = setInterval(runCountdown, 1000);
+
 function runCountdown() {
   const today = new Date();
   const timeLeft = endDate - today;
 
-  console.log("Time left: ", timeLeft);
+  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const min = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  const sec = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  if (days !== daysCard.value) daysCard.textContent = days;
+  if (hours !== hoursCard.value) hoursCard.textContent = hours;
+  if (min !== minCard.value) minCard.textContent = min;
+  if (sec !== secCard.value) secCard.textContent = sec;
+
+  if (timeLeft < 0) clearInterval(timer);
 }
 
 // setInterval(runCountdown, 1000);
