ef900d18332731829e989e01ce74ed90259441fe
jmmarketing <jeff@jeffreymclean.com>
2025-08-06 16:47:10 -0700

    Had to update the navigo router config to look at window path to determine the router basepath. If github should use a different url config than then localhost.

--- src/js/router.js
+++ src/js/router.js
@@ -3,7 +3,8 @@ import * as controller from "./controller";
 
 class Router {
   constructor() {
-    this.router = new Navigo("/", { hash: false, strategy: "ONE" });
+    const basePath = this._getBasePath();
+    this.router = new Navigo(basePath, { hash: false, strategy: "ONE" });
     this._setupRoutes();
   }
 
@@ -11,6 +12,23 @@ class Router {
     this.router
       .on("/", () => controller.showHome())
       .on("/signup", () => controller.showSignup());
+  }
+
+  _getBasePath() {
+    // Check if we're on GitHub Pages or similar
+    const hostname = window.location.hostname;
+    const pathname = window.location.pathname;
+
+    // Local development
+    if (hostname === "localhost" || hostname === "127.0.0.1") return "/";
+
+    // GitHub Pages pattern: username.github.io/repo-name/
+    if (hostname.includes("github.io")) {
+      const pathParts = pathname.split("/").filter((part) => part);
+      if (pathParts.length > 0) return `/${pathParts[0]}/`;
+    }
+    // Custom domain or other hosting
+    return "/";
   }
 
   init() {
